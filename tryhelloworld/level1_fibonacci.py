79591594bf6ab50d0fb6e2f8cc4fa9b837dfe7b3
ysoftman <ysoftman@gmail.com>
2017-07-28 21:59:14 +0900

    재귀방법 추가

--- tryhelloworld/level1_fibonacci.py
+++ tryhelloworld/level1_fibonacci.py
@@ -3,6 +3,15 @@
 
 # 피보나치 수는 F(0) = 0, F(1) = 1일 때, 2 이상의 n에 대하여 F(n) = F(n-1) + F(n-2) 가 적용되는 점화식입니다. 2 이상의 n이 입력되었을 때, fibonacci 함수를 제작하여 n번째 피보나치 수를 반환해 주세요. 예를 들어 n = 3이라면 2를 반환해주면 됩니다.
 
+
+def fibonacci_recursive(num):
+    if num == 0:
+        return 0
+    elif num == 1:
+        return 1
+    return fibonacci_recursive(num - 1) + fibonacci_recursive(num - 2)
+
+
 def fibonacci(num):
     # if num < 2:
     #     return num
@@ -16,7 +25,13 @@ def fibonacci(num):
 
     return out
 
+
 # 아래는 테스트로 출력해 보기 위한 코드입니다.
+print(fibonacci_recursive(3))
+print(fibonacci_recursive(4))
+print(fibonacci_recursive(5))
+print(fibonacci_recursive(6))
+print("-----")
 print(fibonacci(3))
 print(fibonacci(4))
 print(fibonacci(5))
