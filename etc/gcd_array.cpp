e37e31d6eefe29bfc1d1e0b046a234704c1e9d8b
ysoftman <ysoftman@gmail.com>
2019-10-11 18:35:44 +0900

    update

---
+++ etc/gcd_array.cpp
@@ -0,0 +1,39 @@
+// https://www.geeksforgeeks.org/gcd-two-array-numbers/
+/*
+배열내의 모든 값들의 gcd(최대공약수) 구하기
+*/
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int gcd(int a, int b)
+{
+    if (b == 0)
+    {
+        return a;
+    }
+    return gcd(b, a % b);
+}
+
+int gcd_arrary(int *arr, int len)
+{
+    int max_gcd = 1;
+    for (int i = 0; i < len; ++i)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    max_gcd = arr[0];
+    for (int i = 1; i < len; ++i)
+    {
+        max_gcd = gcd(max_gcd, arr[i]);
+    }
+    return max_gcd;
+}
+
+int main()
+{
+    int arr[] = {88, 80, 14, 64, 32};
+    int len = 5;
+    cout << gcd_arrary(arr, len) << endl;
+}
