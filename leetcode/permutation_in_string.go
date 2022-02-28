40e0edf2070df3d3deb8134c95f5f1763e7f40d6
ysoftman <ysoftman@gmail.com>
2022-02-28 12:04:35 +0900

    update

--- leetcode/permutation_in_string.go
+++ leetcode/permutation_in_string.go
@@ -1,28 +1,17 @@
 /*
 https://leetcode.com/problems/permutation-in-string/
+
 567. Permutation in String
-Medium
 
-5342
-
-147
-
-Add to List
-
-Share
 Given two strings s1 and s2, return true if s2 contains a permutation of s1, or false otherwise.
-
 In other words, return true if one of s1's permutations is the substring of s2.
 
-
-
 Example 1:
-
 Input: s1 = "ab", s2 = "eidbaooo"
 Output: true
 Explanation: s2 contains one permutation of s1 ("ba").
-Example 2:
 
+Example 2:
 Input: s1 = "ab", s2 = "eidboaoo"
 Output: false
 */
