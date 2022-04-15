6a2d14dcebc4ac47d5a06427229837f40a3e2ef7
ysoftman <ysoftman@gmail.com>
2022-04-15 21:28:51 +0900

    add solution

---
+++ leetcode/reverse_linked_list_ii.go
@@ -0,0 +1,70 @@
+/*
+https://leetcode.com/problems/reverse-linked-list-ii/
+92. Reverse Linked List II
+Medium
+Given the head of a singly linked list and two integers left and right where left <= right, reverse the nodes of the list from position left to position right, and return the reversed list.
+
+Example 1:
+Input: head = [1,2,3,4,5], left = 2, right = 4
+Output: [1,4,3,2,5]
+
+Example 2:
+Input: head = [5], left = 1, right = 1
+Output: [5]
+
+Constraints:
+The number of nodes in the list is n.
+1 <= n <= 500
+-500 <= Node.val <= 500
+1 <= left <= right <= n
+*/
+package main
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func reverseBetween(head *ListNode, left int, right int) *ListNode {
+	base := &ListNode{
+		0,
+		head,
+	}
+	cnt := 0
+	var leftNodePre *ListNode
+	var leftNode *ListNode
+	var ppre *ListNode = base
+	var pre *ListNode = base
+	for head != nil {
+		cnt++
+		if cnt == left {
+			leftNode = head
+			leftNodePre = pre
+		}
+		if cnt == right {
+			leftNodePre.Next = head
+			leftNode.Next = head.Next
+		}
+		ppre = pre
+		pre = head
+		head = head.Next
+		if cnt > left && cnt <= right {
+			pre.Next = ppre
+		}
+	}
+	return base.Next
+}
+
+func main() {
+	head := []int{1, 2, 3, 4, 5}
+	printLinkedList(reverseBetween(makeLinkedList(head), 2, 4))
+
+	head = []int{5}
+	printLinkedList(reverseBetween(makeLinkedList(head), 1, 1))
+
+	head = []int{0, 1, 3, 2, 5, 2, 3, 1, 6}
+	printLinkedList(reverseBetween(makeLinkedList(head), 1, 5))
+
+}
