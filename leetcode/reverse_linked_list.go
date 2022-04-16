f19edf826e7c2e1ac23a863ef3a2a100dc5575b6
ysoftman <ysoftman@gmail.com>
2022-04-17 00:29:12 +0900

    update

--- leetcode/reverse_linked_list.go
+++ leetcode/reverse_linked_list.go
@@ -1,6 +1,7 @@
 /*
 https://leetcode.com/problems/reverse-linked-list
 206. Reverse Linked List
+Easy
 Given the head of a singly linked list, reverse the list, and return the reversed list.
 
 Example 1:
--- leetcode/reverse_string.go
+++ leetcode/reverse_string.go
@@ -1,7 +1,7 @@
 /*
 https://leetcode.com/problems/reverse-string/
-
 344. Reverse String
+Easy
 Write a function that reverses a string. The input string is given as an array of characters s.
 You must do this by modifying the input array in-place with O(1) extra memory.
 
@@ -12,7 +12,6 @@ Output: ["o","l","l","e","h"]
 Example 2:
 Input: s = ["H","a","n","n","a","h"]
 Output: ["h","a","n","n","a","H"]
-
 */
 
 package main
