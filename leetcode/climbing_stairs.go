231766f3aa3c67822c85eafbccad735eac24d9b8
ysoftman <ysoftman@gmail.com>
2022-04-17 00:15:20 +0900

    update

--- leetcode/climbing_stairs.go
+++ leetcode/climbing_stairs.go
@@ -1,23 +1,18 @@
 /*
 https://leetcode.com/problems/climbing-stairs/
-
 70. Climbing Stairs
-
+Easy
 You are climbing a staircase. It takes n steps to reach the top.
-
 Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
 
 Example 1:
-
 Input: n = 2
 Output: 2
 Explanation: There are two ways to climb to the top.
 1. 1 step + 1 step
 2. 2 steps
 
-
 Example 2:
-
 Input: n = 3
 Output: 3
 Explanation: There are three ways to climb to the top.
@@ -26,14 +21,13 @@ Explanation: There are three ways to climb to the top.
 3. 2 steps + 1 step
 
 Constraints:
-
 1 <= n <= 45
 */
 package main
 
 import "fmt"
 
-func climbStairs(n int) int {
+func climbStairs2(n int) int {
 	if n < 0 {
 		return 0
 	}
@@ -59,6 +53,25 @@ func climbStairs(n int) int {
 	fmt.Println("temp:", temp)
 
 	return temp[len(temp)-1]
+}
+
+func climbStairs(n int) int {
+	if n < 0 {
+		return 0
+	}
+	if n == 0 || n == 1 {
+		return 1
+	}
+	if n == 2 {
+		return 2
+	}
+	dp := make([]int, n)
+	dp[0] = 1
+	dp[1] = 2
+	for i := 2; i < n; i++ {
+		dp[i] = dp[i-2] + dp[i-1]
+	}
+	return dp[n-1]
 }
 
 func main() {
