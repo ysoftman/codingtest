1f47d6263de2eb5249c12d9529f760de264fb688
ysoftman <ysoftman@gmail.com>
2022-08-09 03:08:04 +0900

    add solution

---
+++ leetcode/sort_list.go
@@ -0,0 +1,61 @@
+/*
+https://leetcode.com/problems/sort-list/
+148. Sort List
+Medium
+Given the head of a linked list, return the list after sorting it in ascending order.
+
+Example 1:
+Input: head = [4,2,1,3]
+Output: [1,2,3,4]
+
+Example 2:
+Input: head = [-1,5,3,4,0]
+Output: [-1,0,3,4,5]
+
+Example 3:
+Input: head = []
+Output: []
+
+Constraints:
+The number of nodes in the list is in the range [0, 5 * 104].
+-105 <= Node.val <= 105
+
+Follow up: Can you sort the linked list in O(n logn) time and O(1) memory (i.e. constant space)?
+*/
+package main
+
+import "sort"
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+// 정렬된 새로운 ListNode 를 만들기
+// time complexity(nlogn)
+// space complexity(n)
+func sortList(head *ListNode) *ListNode {
+	l := []int{}
+	for head != nil {
+		l = append(l, head.Val)
+		head = head.Next
+	}
+	sort.Ints(l)
+	r := &ListNode{}
+	newhead := r
+	for _, v := range l {
+		r.Next = &ListNode{
+			Val: v,
+		}
+		r = r.Next
+	}
+	return newhead.Next
+}
+
+func main() {
+	printLinkedList(sortList(makeLinkedList([]int{4, 2, 1, 3})))
+	printLinkedList(sortList(makeLinkedList([]int{-1, 5, 3, 4, 0})))
+	printLinkedList(sortList(makeLinkedList([]int{})))
+}
