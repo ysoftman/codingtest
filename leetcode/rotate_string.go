7bb665980d835788c8b992505b2c229c8015dc58
ysoftman <ysoftman@gmail.com>
2023-01-01 01:31:16 +0900

    add solution

---
+++ leetcode/rotate_string.go
@@ -0,0 +1,41 @@
+/*
+https://leetcode.com/problems/rotate-string/
+796. Rotate String
+Easy
+
+Given two strings s and goal, return true if and only if s can become goal after some number of shifts on s.
+A shift on s consists of moving the leftmost character of s to the rightmost position.
+For example, if s = "abcde", then it will be "bcdea" after one shift.
+
+Example 1:
+Input: s = "abcde", goal = "cdeab"
+Output: true
+
+Example 2:
+Input: s = "abcde", goal = "abced"
+Output: false
+
+Constraints:
+1 <= s.length, goal.length <= 100
+s and goal consist of lowercase English letters.
+*/
+
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func rotateString(s string, goal string) bool {
+	if len(s) != len(goal) {
+		return false
+	}
+	s += s
+	return strings.Contains(s, goal)
+}
+
+func main() {
+	fmt.Println(rotateString("abcde", "cdeab"))
+	fmt.Println(rotateString("abcde", "abced"))
+}
