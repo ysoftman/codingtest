569fa3f8ef8a9efaff8efe6dce121f941d73fbd1
ysoftman <ysoftman@gmail.com>
2023-06-29 19:27:29 +0900

    update

--- leetcode/valid_palindrome_ii.go
+++ leetcode/valid_palindrome_ii.go
@@ -1,6 +1,7 @@
 /*
 https://leetcode.com/problems/valid-palindrome-ii/
 680. Valid Palindrome II
+Easy
 Given a string s, return true if the s can be palindrome after deleting at most one character from it.
 
 Example 1:
