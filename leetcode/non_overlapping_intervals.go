c893e948cd1225fe1f0a19e727b816af82e9509a
ysoftman <ysoftman@gmail.com>
2023-07-30 01:38:58 +0900

    add solution

---
+++ leetcode/non_overlapping_intervals.go
@@ -0,0 +1,50 @@
+/*
+https://leetcode.com/problems/non-overlapping-intervals/
+435. Non-overlapping Intervals
+Medium
+Given an array of intervals intervals where intervals[i] = [starti, endi], return the minimum number of intervals you need to remove to make the rest of the intervals non-overlapping.
+
+Example 1:
+Input: intervals = [[1,2],[2,3],[3,4],[1,3]]
+Output: 1
+Explanation: [1,3] can be removed and the rest of the intervals are non-overlapping.
+
+Example 2:
+Input: intervals = [[1,2],[1,2],[1,2]]
+Output: 2
+Explanation: You need to remove two [1,2] to make the rest of the intervals non-overlapping.
+
+Example 3:
+Input: intervals = [[1,2],[2,3]]
+Output: 0
+Explanation: You don't need to remove any of the intervals since they're already non-overlapping.
+*/
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func eraseOverlapIntervals(intervals [][]int) int {
+	// endi 값 순으로 정렬
+	sort.Slice(intervals, func(i, j int) bool {
+		return intervals[i][1] < intervals[j][1]
+	})
+	// fmt.Println(intervals)
+	cnt := 0
+	// starti 값만 이전 intervals[i-1]과 비교해서 오버랩 체크
+	for i := 1; i < len(intervals); i++ {
+		if intervals[i][0] < intervals[i-1][1] {
+			intervals[i] = intervals[i-1]
+			cnt++
+		}
+	}
+	return cnt
+}
+
+func main() {
+	fmt.Println(eraseOverlapIntervals([][]int{{1, 2}, {2, 3}, {3, 4}, {1, 3}}))
+	fmt.Println(eraseOverlapIntervals([][]int{{1, 2}, {1, 2}, {1, 2}}))
+	fmt.Println(eraseOverlapIntervals([][]int{{1, 2}, {2, 3}}))
+}
