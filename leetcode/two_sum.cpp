01872fca5f7dbe3734ac5314171ecd4847fecb77
ysoftman <ysoftman@gmail.com>
2025-09-25 21:07:47 +0900

    update solution

--- leetcode/two_sum.cpp
+++ leetcode/two_sum.cpp
@@ -2,11 +2,9 @@
 https://leetcode.com/explore/featured/card/top-interview-questions-easy/92/array/546/
 
 Given an array of integers, return indices of the two numbers such that they add up to a specific target.
-
 You may assume that each input would have exactly one solution, and you may not use the same element twice.
 
 Example:
-
 Given nums = [2, 7, 11, 15], target = 9,
 
 Because nums[0] + nums[1] = 2 + 7 = 9,
@@ -19,7 +17,7 @@ return [0, 1].
 using namespace std;
 
 // 원소는 한번씩만 보고, O(n) 으로 끝내기
-vector<int> twoSum(vector<int> &nums, int target) {
+vector<int> twoSum(vector<int>& nums, int target) {
     vector<int> result;
     // hashmap O(1)
     unordered_map<int, int> hashmap;
@@ -38,10 +36,48 @@ vector<int> twoSum(vector<int> &nums, int target) {
     return result;
 }
 
+// sort array, and use two pointer, O(logN)
+vector<int> twoSum2(vector<int>& nums, int target) {
+    struct n {
+        int val;
+        int idx;
+        n(int a, int b) : val(a), idx(b) {}
+    };
+    vector<n> data;
+    for (size_t i = 0; i < nums.size(); i++) {
+        data.emplace_back(nums[i], i);
+    }
+    // for (auto& i : data) {
+    //     cout << i.val << "--" << i.idx << endl;
+    // }
+    sort(data.begin(), data.end(), [](const n& a, const n& b) {
+        return a.val < b.val;
+    });
+    int i = 0;
+    int j = data.size() - 1;
+    for (; i < j;) {
+        if (data.at(i).val + data.at(j).val == target) {
+            return vector<int>{data.at(i).idx, data.at(j).idx};
+        }
+        if (data.at(i).val + data.at(j).val < target) {
+            i++;
+        } else if (data.at(i).val + data.at(j).val > target) {
+            j--;
+        }
+    }
+    return vector<int>{};
+}
+
 int main() {
     vector<int> input = {2, 7, 11, 15};
-    vector<int> r     = twoSum(input, 9);
-    for (auto i : r) {
+    cout << "twoSum" << endl;
+    vector<int> r = twoSum(input, 9);
+    for (const auto& i : r) {
+        cout << i << endl;
+    }
+    cout << "twoSum2" << endl;
+    vector<int> r2 = twoSum2(input, 9);
+    for (const auto& i : r2) {
         cout << i << endl;
     }
     return 0;
--- leetcode/two_sum.go
+++ leetcode/two_sum.go
@@ -6,18 +6,16 @@ Given an array of integers nums and an integer target, return indices of the two
 You may assume that each input would have exactly one solution, and you may not use the same element twice.
 You can return the answer in any order.
 
-
 Example 1:
-
 Input: nums = [2,7,11,15], target = 9
 Output: [0,1]
 Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
-Example 2:
 
+Example 2:
 Input: nums = [3,2,4], target = 6
 Output: [1,2]
-Example 3:
 
+Example 3:
 Input: nums = [3,3], target = 6
 Output: [0,1]
 */
@@ -43,27 +41,45 @@ func twoSum(nums []int, target int) []int {
 	return nil
 }
 
+type n struct {
+	val int
+	idx int
+}
+
 // 배열 정렬후, 시작,끝 인덱스를 두고 찾을때
-// if sorted array, using two (index) pointers : log(n)
-func sortedTwoSum(nums []int, target int) []int {
-	sort.Ints(nums)
+// sort array and using two (index) pointers : log(n)
+func twoSum2(nums []int, target int) []int {
+	data := []n{}
+	for i := 0; i < len(nums); i++ {
+		data = append(data, n{nums[i], i})
+	}
+	sort.Slice(data, func(i, j int) bool {
+		return data[i].val < data[j].val
+	})
 	i := 0
-	j := len(nums) - 1
+	j := len(data) - 1
 	for i < j {
-		if nums[i]+nums[j] == target {
-			return []int{i, j}
-		} else if nums[i]+nums[j] < target {
+		if data[i].val+data[j].val == target {
+			return []int{data[i].idx, data[j].idx}
+		}
+		if data[i].val+data[j].val < target {
 			i++
-		} else if nums[i]+nums[j] > target {
+		} else if data[i].val+data[j].val > target {
 			j--
 		}
 	}
-	return nil
+	return []int{}
 }
 
 func main() {
+	fmt.Println("twoSum")
 	fmt.Println(twoSum([]int{2, 7, 11, 15}, 9))
 	fmt.Println(twoSum([]int{3, 2, 4}, 6))
 	fmt.Println(twoSum([]int{3, 3}, 6))
 	fmt.Println(twoSum([]int{-3, 4, 3, 90}, 0))
+	fmt.Println("twoSum2")
+	fmt.Println(twoSum2([]int{2, 7, 11, 15}, 9))
+	fmt.Println(twoSum2([]int{3, 2, 4}, 6))
+	fmt.Println(twoSum2([]int{3, 3}, 6))
+	fmt.Println(twoSum2([]int{-3, 4, 3, 90}, 0))
 }
