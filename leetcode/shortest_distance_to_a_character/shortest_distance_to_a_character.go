9eb76fcacbb5f45ae0aed7a9895e0b1b4ab97409
ysoftman <ysoftman@gmail.com>
2025-11-02 00:34:19 +0900

    add solution

---
+++ leetcode/shortest_distance_to_a_character/shortest_distance_to_a_character.go
@@ -0,0 +1,75 @@
+/*
+https://leetcode.com/problems/shortest-distance-to-a-character
+821. Shortest Distance to a Character
+Easy
+Given a string s and a character c that occurs in s, return an array of integers answer where answer.length == s.length and answer[i] is the distance from index i to the closest occurrence of character c in s.
+
+The distance between two indices i and j is abs(i - j), where abs is the absolute value function.
+
+Example 1:
+Input: s = "loveleetcode", c = "e"
+Output: [3,2,1,0,1,0,0,1,2,2,1,0]
+Explanation: The character 'e' appears at indices 3, 5, 6, and 11 (0-indexed).
+The closest occurrence of 'e' for index 0 is at index 3, so the distance is abs(0 - 3) = 3.
+The closest occurrence of 'e' for index 1 is at index 3, so the distance is abs(1 - 3) = 2.
+For index 4, there is a tie between the 'e' at index 3 and the 'e' at index 5, but the distance is still the same: abs(4 - 3) == abs(4 - 5) = 1.
+The closest occurrence of 'e' for index 8 is at index 6, so the distance is abs(8 - 6) = 2.
+
+Example 2:
+Input: s = "aaab", c = "b"
+Output: [3,2,1,0]
+
+Constraints:
+1 <= s.length <= 104
+s[i] and c are lowercase English letters.
+It is guaranteed that c occurs at least once in s.
+*/
+package main
+
+import "fmt"
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func abs(a int) int {
+	if a < 0 {
+		return -a
+	}
+	return a
+}
+
+func shortestToChar(s string, c byte) []int {
+	targetIndices := []int{1<<31 - 1}
+	for i := range s {
+		if s[i] == c {
+			targetIndices = append(targetIndices, i)
+		}
+	}
+	r := []int{}
+	tidx := 0
+	for i := range s {
+		dist1 := abs(i - targetIndices[tidx])
+		if tidx+1 < len(targetIndices) {
+			dist2 := abs(i - targetIndices[tidx+1])
+			r = append(r, min(dist1, dist2))
+			if dist2 == 0 {
+				tidx++
+			}
+			continue
+		}
+		r = append(r, dist1)
+		if dist1 == 0 {
+			tidx++
+		}
+	}
+	return r
+}
+
+func main() {
+	fmt.Println(shortestToChar("loveleetcode", 'e'))
+	fmt.Println(shortestToChar("aaab", 'b'))
+}
